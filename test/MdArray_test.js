d87f2725234df4a2beb040d36d7856a3714750c2
Michael Webster <mikewobbler@gmail.com>
2016-01-17 00:55:04 +1100

    Added some more operations and Transpose.

--- test/MdArray_test.js
+++ test/MdArray_test.js
@@ -24,7 +24,7 @@ var MdArray = require('../lib/MdArray.js');
 
 exports.testCreate1DNoSizeGiven = function(test) {
     var testD = _us.range(11);
-    var testArray = new MdArray(testD);
+    var testArray = new MdArray({data : testD });
     var passed = _us.reduce(testD, function(memo, val, idx) {
 	return memo && (val == testArray.get(idx));
     }, true);
@@ -35,7 +35,7 @@ exports.testCreate1DNoSizeGiven = function(test) {
 
 exports.testCreate1DSizeGiven = function(test) {
     var testD = _us.range(11);
-    var testArray = new MdArray(testD, 11);
+    var testArray = new MdArray({data :testD, shape: [11]});
     var passed = _us.reduce(testD, function(memo, val, idx) {
 	return memo && (val == testArray.get(idx));
     }, true);
@@ -46,12 +46,79 @@ exports.testCreate1DSizeGiven = function(test) {
 
 exports.testCreate2DSizeGiven = function(test) {
     var testD = _us.range(6);
-    var testArray = new MdArray(testD, 3,2);
+    var testArray = new MdArray({data : testD, shape: [3,2]});
     var passed = _us.reduce(testD, function(memo, val, idx) {
 	return memo && (val == testArray.get(Math.floor(idx/2), idx % 2));
     }, true);
     test.ok(passed,
 	    "test2DCreate: Error creating and testing values in 2D MdArray.");
+    test.done();
+};
+
+exports.testZeros = function(test) {
+    var testArray = MdArray.zeros({shape: [3,2]});
+    var flattened = testArray.flatten();
+    var passed = _us.reduce(flattened, function(memo, val, idx) {
+	return memo && (val == testArray.get(Math.floor(idx/2), idx % 2));
+    }, true);
+    test.ok(passed,
+	    "testZeros: Error creating and testing values in 2D array of Zeros.");
+    test.done();
+};
+
+exports.testOnes = function(test) {
+    var testArray = MdArray.ones({shape: [3,2]});
+    var flattened = testArray.flatten();
+    var passed = _us.reduce(flattened, function(memo, val, idx) {
+	return memo && (val == testArray.get(Math.floor(idx/2), idx % 2));
+    }, true);
+    test.ok(passed,
+	    "testOnes: Error creating and testing values in 2D array of Ones.");
+    test.done();
+};
+
+exports.testArange = function(test) {
+    var testArray = MdArray.arange({end: 50, shape : [5 ,10]});
+    var testVal = _us.range(50);
+    var passed = _us.reduce(testVal, function(memo, val, idx) {
+	return memo && (val == testArray.get(Math.floor(idx/10), idx % 10));
+    }, true);
+    test.ok(passed,
+	    "testaRange: Error creating and testing values in aRange 2D array.");
+    test.done();
+};
+
+
+exports.testPlus = function(test) {
+    var testArray1 = MdArray.ones({shape: [5, 10]});
+    var testArray2 = MdArray.ones({shape: [5, 10]});
+    var testArray3 = testArray1.add(testArray2);
+    var d = testArray3.data;
+    var passed = _us.every(d, function(x) { return x === 2; });
+    test.ok(passed,
+	    "testPlus: Error adding 2 2D arrays.");
+    test.done();
+};
+
+exports.testSub = function(test) {
+    var testArray1 = MdArray.ones({shape: [5, 10]});
+    var testArray2 = MdArray.ones({shape: [5, 10]});
+    var testArray3 = testArray1.sub(testArray2);
+    var d = testArray3.data;
+    var passed = _us.every(d, function(x) { return x === 0; });
+    test.ok(passed,
+	    "testPlus: Error adding 2 2D arrays.");
+    test.done();
+};
+
+exports.testMul = function(test) {
+    var testArray1 = MdArray.ones({shape: [5, 10]}).mul(2);
+    var testArray2 = MdArray.ones({shape: [5, 10]}).mul(2);
+    var testArray3 = testArray1.mul(testArray2);
+    var d = testArray3.data;
+    var passed = _us.every(d, function(x) { return x === 4; });
+    test.ok(passed,
+	    "testPlus: Error adding 2 2D arrays.");
     test.done();
 };
 
